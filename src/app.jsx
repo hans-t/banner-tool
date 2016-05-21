e3486991fd6583cabba7278aa1904ebd387428e4
hans-t <hans.tunggajaya@gmail.com>
2016-05-21 09:25:59 +0800

    Implemented simple router

--- src/app.jsx
+++ src/app.jsx
@@ -8,23 +8,52 @@ import SourcesView from './sources-page/view';
 import AddCopiesView from './add-copies-page/view';
 import EditCopiesView from './edit-copies-page/view';
 import ResultsView from './results-page/view';
+import TemplateEditor from './template-editor/view';
 
 
 const pages = [
-  <IndexView />,
-  <SourcesView />,
-  <AddCopiesView />,
-  <EditCopiesView />,
-  <ResultsView />,
+  IndexView,
+  SourcesView,
+  AddCopiesView,
+  EditCopiesView,
+  ResultsView,
 ];
 
 
-// component for glueing components from different domain
-const App = ({ currentPageNum }) => (
-  <MuiThemeProvider muiTheme={getMuiTheme()}>
-    <div>{pages[currentPageNum]}</div>
-  </MuiThemeProvider>
-);
+function router({ currentPageNum }, { route }) {
+  switch (route) {
+    case '/editor':
+      return TemplateEditor;
+
+    default:
+      return pages[currentPageNum];
+  }
+}
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      route: window.location.hash.substr(1),
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('hashchange', () => this.setState({
+      route: window.location.hash.substr(1),
+    }));
+  }
+
+  render() {
+    const Child = router(this.props, this.state);
+    return (
+      <MuiThemeProvider muiTheme={getMuiTheme()}>
+        <Child />
+      </MuiThemeProvider>
+    );
+  }
+}
 
 App.propTypes = {
   currentPageNum: React.PropTypes.number.isRequired,
--- src/index.jsx
+++ src/index.jsx
@@ -7,7 +7,6 @@ import injectTapEventPlugin from 'react-tap-event-plugin';
 import rootReducer from './reducers';
 import initialState from './initialState';
 import App from './app';
-import TemplateEditor from './template-editor';
 
 import '../static/css/fonts.css';
 
@@ -15,25 +14,15 @@ import '../static/css/fonts.css';
 injectTapEventPlugin();
 
 
-// stupid way of routing, will need to refactor this in the future.
-const { pathname } = window.location;
-if (pathname === '/') {
-  ReactDOM.render(
-    <Provider
-      store={createStore(
-        rootReducer,
-        initialState,
-        window.devToolsExtension ? window.devToolsExtension() : undefined
-      )}
-    >
-      <App />
-    </Provider>,
-    document.getElementById('app')
-  );
-} else if (pathname === '/editor') {
-  ReactDOM.render(
-    <TemplateEditor />,
-    document.getElementById('app')
-  );
-}
-
+ReactDOM.render(
+  <Provider
+    store={createStore(
+      rootReducer,
+      initialState,
+      window.devToolsExtension ? window.devToolsExtension() : undefined
+    )}
+  >
+    <App />
+  </Provider>,
+  document.getElementById('app')
+);
