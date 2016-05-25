cc95a39b79eb06c353bed9f39fd8bffe89092770
Zalora-hans <hans.t@zalora.com>
2016-05-25 10:59:59 +0800

    Corrected wrong btn text

--- src/banner-settings-page/nav-buttons.jsx
+++ src/banner-settings-page/nav-buttons.jsx
@@ -24,7 +24,7 @@ function dispatcher(dispatch, state) {
 }
 
 
-const LeftBtn = prevBtnFactory({ label: 'Edit copies' });
+const LeftBtn = prevBtnFactory({ label: 'Edit banner settings' });
 const RightBtn = nextBtnFactory({
   label: 'See results',
   validator,
